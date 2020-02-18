131eaec485f1d357a385889c5ac3000eff0a5fa3
Aimeric David <david.aimeric@outlook.fr>
2020-02-18 17:13:06 +0100

    Push avec bug pour Denis

--- app/src/main/java/aimeric/david/meetingapp/Dialog/CreateMeeting.java
+++ app/src/main/java/aimeric/david/meetingapp/Dialog/CreateMeeting.java
@@ -1,22 +1,60 @@
 package aimeric.david.meetingapp.Dialog;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.text.format.DateFormat;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import java.util.Calendar;
+
 import aimeric.david.meetingapp.R;
 
 public class CreateMeeting extends DialogFragment {
+
+    private Button buttonHour;
+    private EditText editTest;
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_create_meeting, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_create_meeting, container, false);
+
+        Calendar calendar = Calendar.getInstance();
+
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+
+        buttonHour = view.findViewById(R.id.heure_button);
+        editTest = view.findViewById(R.id.intitule_editText);
+
+        buttonHour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+
+                    }
+                },hour, minute, android.text.format.DateFormat.is24HourFormat(this));
+
+            }
+        });
+
+        return view;
+
     }
 }
